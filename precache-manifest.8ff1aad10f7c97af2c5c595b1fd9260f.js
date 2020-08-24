3ca1607335d150b82fa085dcb79935799118dfa0
fiasco <scolardf@tcd.ie>
2020-08-24 17:26:48 +0100

    Updates

---
+++ precache-manifest.8ff1aad10f7c97af2c5c595b1fd9260f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f3f648ea7662bcb3e919c06c9863e70",
+    "url": "/fiasco-io/index.html"
+  },
+  {
+    "revision": "be31c0996767e5ef3445",
+    "url": "/fiasco-io/static/css/main.61944c20.chunk.css"
+  },
+  {
+    "revision": "de460ca29a19f5e842be",
+    "url": "/fiasco-io/static/js/2.5ffe0b1b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fiasco-io/static/js/2.5ffe0b1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be31c0996767e5ef3445",
+    "url": "/fiasco-io/static/js/main.0e70a7ee.chunk.js"
+  },
+  {
+    "revision": "33f383ab01b05fa9c534",
+    "url": "/fiasco-io/static/js/runtime-main.a0bd8ce2.js"
+  }
+]);
