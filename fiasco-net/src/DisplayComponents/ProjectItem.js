8df450450388f0cde70dc31be94ccc3f3993ba24
Fiach Scolard <scolardf@tcd.ie>
2020-08-26 18:33:44 +0100

    ProjectItem Framework
    Fleshed out structure of ProjectItem component, added .css tags

--- fiasco-net/src/DisplayComponents/ProjectItem.js
+++ fiasco-net/src/DisplayComponents/ProjectItem.js
@@ -4,13 +4,28 @@ class ProjectItem extends React.Component {
     constructor(props) {
         super(props);
 
-        
+        this.state = {
+            projectObject: "",
+        }
+    }
+
+    componentDidMount() {
+        this.setState({projectObject: this.props,})
     }
 
     render() {
+        const projectObject = this.state.projectObject;
         return(
             <div id="projectItemContainer">
-                <p>This is a ProjectItem</p>
+                <div id="projectTitleLine">
+        <p id="dateTime">{projectObject.dateTime}</p>
+                    <p id="titleP">{projectObject.title}</p>
+                </div>
+                <div id="imageContainer">{projectObject.image}</div>
+                <div id="descriptionAndRepo">
+                    <p id="descriptionP">{projectObject.description}</p>
+                    <p id="repoP">Repo: <a href="#">{projectObject.repoLink}</a></p>
+                </div>
             </div>
         );
     }
